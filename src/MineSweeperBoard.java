96ad1deb9186979564c133b4ce5bebbc3b369dde
SeulgiLee12 <ylesiw1209@gmail.com>
2023-11-13 01:57:47 +0900

    1

--- src/MineSweeperBoard.java
+++ src/MineSweeperBoard.java
@@ -2,92 +2,102 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class MineSweeperBoard {
-    int input = 0;
-    int mine_num = 0;
-    int mine_pos[] = {-1,0,1};
-    int mine_around = 0;
+    int BOARD_ROW = 0;
+    int MINE_NUM = 0;
+    int patternForCheckMines[] = {-1,0,1};
+    int MINE_AROUND = 0;
     String board[][];
     int minePositions[];
     Scanner scan = new Scanner(System.in);
-    public void inputMineNums() {
+    Random random = new Random();
+
+    public void initialize() {
         // 게임보드 크기랑 지뢰개수 입력 받기
         try {
-            while (true) {
-                System.out.println("input row number : ");
-                input = scan.nextInt();
-                if (input <= 4 || input >= 16) System.out.println("invalid number");
-                else break;
-            }
-            while (true) {
-                System.out.println("input mine number : ");
-                mine_num = scan.nextInt();
-                if (Math.floor(input*input*0.1) > mine_num || Math.floor(input*input*0.2) < mine_num)
-                    System.out.println("invalid number");
-                else break;
-            }
+            setBoardRow();
+            setMineNum();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public void makeBoard() {
-        board = new String[input][input];
-        for (int i=0; i<input; i++) {
-            for (int j=0; j<input; j++) {
+    public void makeEmptyBoard() {
+        board = new String[BOARD_ROW][BOARD_ROW];
+        for (int i=0; i<BOARD_ROW; i++) {
+            for (int j=0; j<BOARD_ROW; j++) {
                 board[i][j] = "O";
             }
         }
     }
     public void assignMinesPosition() {
-        minePositions = new int[mine_num];
-        Random random = new Random();
-        for (int i=0; i<mine_num; i++) {
-            minePositions[i] = random.nextInt(input,input*input);
-            for (int j=0; j<i; j++) { // 중복검사
-                if (minePositions[i] == minePositions[j]) {
-                    minePositions[i] = random.nextInt(input,input*input);
-                    break;
-                }
-            }
+        minePositions = new int[MINE_NUM];
+        for (int i=0; i<MINE_NUM; i++) {
+            minePositions[i] = random.nextInt(BOARD_ROW,BOARD_ROW*BOARD_ROW);
+            for (int j=0; j<i; j++) // 중복검사
+                if (minePositions[i] == minePositions[j]) i--;
         }
     }
     public void markMines() {
-        for (int n: minePositions) {
-            int a = n/input; int b = n%input;
-            board[a][b] = "X";
+        for (int mineCell: minePositions) {
+            int row = mineCell/BOARD_ROW; int column = mineCell%BOARD_ROW;
+            board[row][column] = "X";
         }
     }
-    public void markMinesAroundCell() {
+    public void markNumOfMinesAroundCell() {
+        for (int row=0; row<BOARD_ROW; row++) {
+            for (int column=0; column<BOARD_ROW; column++) {
+                if (board[row][column] == "X") continue;
+                board[row][column] = Integer.toString(countMinesAroundCell(row, column));
 
-        for (int i=0; i<input*input; i++) {
-            if (board[i/input][i%input] == "X") continue;
-            for (int n: mine_pos) {
-                for (int m: mine_pos) {
-                    if (i/input+n >= 0 && i/input+n < input && i%input+m >= 0 && i%input+m < input) {
-                        if (board[i/input+n][i%input+m] == "X") mine_around++;
-                    }
-                }
             }
-            board[i/input][i%input] = Integer.toString(mine_around);
-            mine_around = 0;
-        }}
+        }
+    }
     public void printBoard() {
-        for (int i=0; i<input; i++) {
-            for (int j=0; j<input; j++) {
-                System.out.print(board[i][j]);
+        for (int row=0; row<BOARD_ROW; row++) {
+            for (int column=0; column<BOARD_ROW; column++) {
+                System.out.print(board[row][column]);
             }
             System.out.println();
         }
     }
 
-    public void makeAndPrintBoard() {
-        inputMineNums();
-        makeBoard();
-        assignMinesPosition();
-        markMines();
-        markMinesAroundCell();
-        printBoard();
+    public void setMineNum() {
+        while (true) {
+        System.out.println("BOARD_ROW mine number : ");
+        MINE_NUM = scan.nextInt();
+        if (Math.floor(BOARD_ROW*BOARD_ROW*0.1) > MINE_NUM || Math.floor(BOARD_ROW*BOARD_ROW*0.2) < MINE_NUM)
+            System.out.println("invalid number");
+        else break;
+        }
+    }
+    public void setBoardRow() {
+        while (true) {
+            System.out.println("BOARD_ROW row number : ");
+            BOARD_ROW = scan.nextInt();
+            if (BOARD_ROW <= 4 || BOARD_ROW >= 16) System.out.println("invalid number");
+            else break;
+        }
+    }
+    public int countMinesAroundCell(int row, int col) {
+        MINE_AROUND = 0;
+        for (int prow: patternForCheckMines) {
+            for (int pcolumn: patternForCheckMines) {
+                if (row+prow >= 0 && row+prow < BOARD_ROW && col+pcolumn >= 0 && col+pcolumn < BOARD_ROW) {
+                    if (board[row+prow][col+pcolumn] == "X") MINE_AROUND++;
+                }
+            }
+        }
+        return MINE_AROUND;
     }
 
+    public void makeAndPrintBoard() {
+        initialize();
+        makeEmptyBoard();
+        assignMinesPosition();
+        markMines();
+        markNumOfMinesAroundCell();
+        printBoard();
+    }
 
     public static void main(String[] args) {
         MineSweeperBoard m = new MineSweeperBoard();
